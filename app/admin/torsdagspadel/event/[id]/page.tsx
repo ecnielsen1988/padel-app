6240ebe237f2b6e00f7a9be0d49faa801d6d5ea7
ecnielsen1988 <ecnielsen1988@gmail.com>
2025-11-04 15:29:47 +0100

    Opdateret startside

--- app/admin/torsdagspadel/event/[id]/page.tsx
+++ app/admin/torsdagspadel/event/[id]/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic';
 import EventAdminTorsdagClient from './EventAdminTorsdagClient';
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <EventAdminTorsdagClient eventId={params.id} />;
+export default function Page({ params }: any) {
+  return <EventAdminTorsdagClient eventId={params.id as string} />;
 }
 
